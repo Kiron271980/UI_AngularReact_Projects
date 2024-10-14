6e77ec9692a771006d42163c84835eac1d34bf1c
Kiran Mayee <sree.kironmai@gmail.com>
2024-10-14 00:08:37 -0400

    refactor App.js

--- reactpracticeapp/src/App.js
+++ reactpracticeapp/src/App.js
@@ -8,6 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>welcome to react programming
+          
+            Developing React applications.
+            using gitHub.
+            compiling and running the applications.
+            </h1>
         </p>
         <a
           className="App-link"
